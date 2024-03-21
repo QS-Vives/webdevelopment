c40bedc2be07afb3a87197ab662e6c936536cf8d
QS-Vives <quinten.steeland@student.vives.be>
2024-03-21 19:26:41 +0100

    Opdracht Extra oefening Nodes 2 verbeteren, effectief de opdracht volgen

--- Semester 2/Labo 6/Extra oefening Nodes 3/scripts/index.js
+++ Semester 2/Labo 6/Extra oefening Nodes 3/scripts/index.js
@@ -1,4 +1,8 @@
 const setup = () => {
+    document.getElementById("input_button").addEventListener("click", append_paragraph);
+}
+
+const append_paragraph = () => {
     let div_element = document.getElementById("myDIV")
     let paragraph_element = document.createElement("p");
 
