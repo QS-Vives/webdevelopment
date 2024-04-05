794bb3509067bc11feddc0397ce52954e5c22cac
QS-Vives <quinten.steeland@student.vives.be>
2024-04-05 22:09:48 +0200

    Opdracht hit an object afwerken

--- Semester 2/Labo 7/hit an object/scripts/index.js
+++ Semester 2/Labo 7/hit an object/scripts/index.js
@@ -8,15 +8,12 @@ let global = {
     timeout_id: 0 // id van de timeout timer, zodat we die kunnen annuleren
 };
 
-
 const setup = () => {
-    document.getElementById("image").addEventListener("click", image_clicked_handler);
-
-    reset();
+    document.getElementById("restart").addEventListener("click", reset);
 }
 
 const image_clicked_handler = () => {
-    clearInterval(global.timeout_id)
+    clearInterval(global.timeout_id);
     let image = document.getElementById("image");
     
     if (image.src.includes(`/${global.IMAGE_PATH_PREFIX}0${global.IMAGE_PATH_SUFFIX}`)) {
@@ -24,25 +21,31 @@ const image_clicked_handler = () => {
         return;
     }
 
-    global.score += 1;
-
+    global.score++;
     update_score_display();
-
     move_and_change_image();
-
     reset_timer();
 }
 
 const update_score_display = () => {
-    document.getElementById("score_display").innerText = + global.score
+    document.getElementById("score_display").innerText = global.score;
 }
 
 const move_and_change_image = () => {
     let image = document.getElementById("image");
 
-    random_number = Math.round(Math.random() * (global.IMAGE_COUNT - 1));
+    let random_number = Math.round(Math.random() * (global.IMAGE_COUNT - 1));
 
-    image.src = `./${global.IMAGE_PATH_PREFIX}${random_number}${global.IMAGE_PATH_SUFFIX}`
+    image.src = `./${global.IMAGE_PATH_PREFIX}${random_number}${global.IMAGE_PATH_SUFFIX}`;
+
+    let canvas = document.getElementById("play_field");
+
+    image.style.marginLeft = `${get_new_margin(canvas.clientWidth)}px`;
+    image.style.marginTop = `${get_new_margin(canvas.clientHeight)}px`;
+}
+
+const get_new_margin = (max_distance) => {
+    return Math.round(Math.random() * (max_distance - global.IMAGE_SIZE));
 }
 
 const reset_timer = () => {
@@ -52,9 +55,24 @@ const reset_timer = () => {
 const reset = () => {
     global.score = 0;
     reset_timer();
+
+    document.getElementById("restart").style.display = "none";
+    document.getElementById("display").style.position = "absolute"
+
+    move_and_change_image();
+
+    document.getElementById("image").addEventListener("click", image_clicked_handler);
 }
 
 const game_over = () => {
+    let image = document.getElementById("image");
+    image.style.marginLeft = image.style.marginTop = "";
+    image.removeEventListener("click", image_clicked_handler);
+
+    document.getElementById("display").style.position = "relative";
+
+    document.getElementById("restart").style.display = "";
+
     alert("Game over!");
 }
 
